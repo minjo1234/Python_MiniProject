91edfbd3094fb482f5e3dc078d5a967006c3b846
Jo Min <122578400+minjo1234@users.noreply.github.com>
2024-03-05 17:49:13 +0900

    memory Game Input

--- memory.py
+++ memory.py
@@ -1,45 +1,58 @@
-import time
 import random
+import time
 
 def memoryGame():
+    level = [6, 8, 10]
 
-    num_length = [6, 8, 10]
-
-    print('기억력 게임을 시작합니다.')
+    print("\n************* 기억력 테스트 게임 *******************")
     time.sleep(1.5)
-    print('게임에서는 총 6개 8개 10개의 숫자가 등장합니다.')
+    print('숫자가 나온 순서대로 입력해주세요.')
     time.sleep(1.5)
-    print('숫자사이의 공백을 유지해주세요!')
+    print('숫자 사이에 스페이스바 , 완료 후 엔터')
     time.sleep(1.5)
 
-    for length in num_length:
-        answer = []
-        input_num = []
-        time.sleep(1.5)
+    for lv in level:
+        printRandomNum(lv)
 
-        for i in range(length):
-            n = random.randint(0, 9)
-            answer.append(n)
-            print(' '*i, n, end='\r')
-            time.sleep(0.5)
+        user_input = input('정답 >>> ')
 
-        while True:
-            try:
-                input_num = list(map(int, input('정답 >>> ').strip().split(' ')))   
-                if input_num == answer:
-                    if length != num_length[-1]:
-                        print("정답입니다! 다음 단계로")
-                    else:
-                        break
-                else:   
-                    print('틀렸습니다ㅠㅠ')
-                    print('정답은' , answer , '입니다.')
-                    break
-            except Exception as arr:
-                print("숫자를 입력해주세요")
-                print("정답 >>> ")        
+        answerCheck(user_input , answer)
 
+        if success == False:
+            break
+        if lv != level[-1]:
+            print('다음 단계로 !')
+            time.sleep(1)
+
+
+def printRandomNum(lv):
+    global answer 
+    answer = []
+
+    for i in range(lv):
+        n = random.randint(0, 9)
+        answer.append(n)
+        print(' '*i, n, end='\r')
+        time.sleep(0.5)
+
+def answerCheck(user_input , answer):
+    global success
+    try:
+        user_input = list(map(int , user_input.strip().split(' ')))
+        if user_input == answer:
+            print('정답입니다!')
+            time.sleep(1)
+            success = True
+        else:
+            print('틀렸습니다.')
+            time.sleep(1)
+            print('정답은' , answer , '입니다.')
+            time.sleep(1)
+            success = False
+    except:
+        print('숫자와 스페이스바만 신경써 주세요 !')
+        user_input = input('정답 >>> ')
+        answerCheck(user_input , answer)
 
 
 memoryGame()
-            
